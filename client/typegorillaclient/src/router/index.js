68a6cb7e519785d782fa2ee766ee5619bb0a2a20
S4more <guimsc@hotmail.com>
2021-08-27 14:55:01 -0400

    Merge conflict resolution

---
+++ client/typegorillaclient/src/router/index.js
@@ -0,0 +1,48 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var vue_router_1 = require("vue-router");
+var Home_vue_1 = __importDefault(require("../views/Home.vue"));
+var routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home_vue_1.default
+    },
+    {
+        path: '/games',
+        name: 'GamesPage',
+        component: function () { return Promise.resolve().then(function () { return __importStar(require('../views/GamesPage.vue')); }); }
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: function () { return Promise.resolve().then(function () { return __importStar(require('../views/About.vue')); }); }
+    }
+];
+var router = vue_router_1.createRouter({
+    history: vue_router_1.createWebHashHistory(),
+    routes: routes
+});
+exports.default = router;
